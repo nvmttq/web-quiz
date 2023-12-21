639f2c7798ae1894563ef276eccdaddb0a8adcea
nvmttq <nvmttq@gmail.com>
2023-12-21 13:08:05 +0700

    fix question undf

--- fetchCNXHKH.js
+++ fetchCNXHKH.js
@@ -1,66 +1,64 @@
-
-
-
 const formatText = (text) => {
-// Replace multiple spaces with single space
-let newText = text.trim().split(/[\s]+/).join(' ');
+  // Replace multiple spaces with single space
+  let newText = text.trim().split(/[\s]+/).join(" ");
 
-// Display the result
-return newText
-}
-
+  // Display the result
+  return newText;
+};
 
 let obj = {
-no: "",
-ques: "",
-choices: [],
-ans: "",
-}
+  no: "",
+  ques: "",
+  choices: [],
+  ans: "",
+};
 
 let reset = {
-no: "",
-ques: "",
-choices: [],
-ans: "",
-}
+  no: "",
+  ques: "",
+  choices: [],
+  ans: "",
+};
 
 const questions = [];
 let heading = 0;
-let noQues = 0, noAns = 0;
+var noQues = 0,
+  noAns = 0;
 var all = document.querySelectorAll("body div > p");
 
-all.forEach(p => {
-const split = p.textContent.split(".");
-const slice = p.textContent.slice(p.textContent.indexOf(".") + 1);
-const text = formatText(slice);
-const num = parseInt(split[0]);
-
-if((num-1) % 5 === 0) {
-    if(noQues > 0) {
-        questions.push(obj);
+all.forEach((p) => {
+  const split = p.textContent.split(".");
+  const slice = p.textContent.slice(p.textContent.indexOf(".") + 1);
+  const text = formatText(slice);
+  if(text === "") return;
+  if (noQues % 5 === 0) {
+    if (noQues > 0) {
+      questions.push(obj);
     }
     obj = {
-        no: "",
-        ques: "",
-        choices: [],
-        ans: "",
+      no: "",
+      ques: "",
+      choices: [],
+      ans: "",
     };
-    noQues++;
     obj.ques = text;
-    obj.no = noQues;
+    obj.no = noQues / 5 + 1;
+    noQues++;
 } else {
     obj.choices.push(text);
 
     let isAns = false;
-    p.querySelectorAll("span").forEach(span => {
-        if(span.style.color === "red") {
-            obj.ans = text;
-            noAns++;
-        }
+    p.querySelectorAll("span").forEach((span) => {
+      if (span.style.color === "red") {
+        obj.ans = text;
+        noAns++;
+      }
     });
-}
+    noQues++;
+  }
+
 });
 
-obj.no = noQues;
+obj.no = noQues / 5 + 1;
 questions.push(obj);
 console.log(noQues, noAns);
